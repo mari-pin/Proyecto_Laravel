a3a1fdf0174400adb931957408573b7de2532d34
mari-pin <marieta_nena@hotmail.com>
2025-02-10 12:26:20 +0100

    carrito_controlle, api controller, token, usuario añadido api_token

--- carrito/database/migrations/2025_02_10_110150_create_carritos_table.php
+++ carrito/database/migrations/2025_02_10_110150_create_carritos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateCarritosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('carritos', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('id_user');
+            $table->string('id_producto');
+            $table->string('nombre_producto');
+            $table->integer('cantidad_producto');
+            $table->float('precio_producto');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('carritos');
     }
 }
