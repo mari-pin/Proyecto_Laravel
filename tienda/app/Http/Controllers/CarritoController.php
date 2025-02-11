f29003d7dc83a0f043568edc79359e2af1fe1176
mari-pin <marieta_nena@hotmail.com>
2025-02-11 12:12:49 +0100

    carritoController tienda hecho

--- tienda/app/Http/Controllers/CarritoController.php
+++ tienda/app/Http/Controllers/CarritoController.php
@@ -16,4 +16,22 @@ class CarritoController extends Controller
         return $response->body();
 
     }
+
+    public function putCarrito(){
+        $response = Http::withToken('nrDd0neP7q')->put('http://carrito/api/carrito/'. auth()->user()->id, ['id_producto'=> 5, 'cantidad_producto'=>2]);
+        return $response->body();
+    }
+
+
+    public function postCarrito(){
+        $response = Http::withToken('nrDd0neP7q')->post('http://carrito/api/carrito/', ['id_user' => auth()->user()->id, 'id_producto'=> 2,'nombre_producto'=>'pantalon', 'precio_producto' => 20, 'cantidad_producto' => 4]);
+        return $response->body();
+    }
+
+
+    public function deleteCarrito(){
+        $response = Http::withToken('nrDd0neP7q')->delete('http://carrito/api/carrito/'. auth()->user()->id);
+        return $response->body();
+    }
 }
+
