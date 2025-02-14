b86ace21b5eeaab8ca2a83cdbb98f4249999b221
mari-pin <marieta_nena@hotmail.com>
2025-02-14 13:30:33 +0100

    finalizado carrito controller

--- tienda/app/Http/Controllers/CarritoController.php
+++ tienda/app/Http/Controllers/CarritoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -13,26 +14,41 @@ class CarritoController extends Controller
 
         $response = Http::withToken('nrDd0neP7q')->get('http://carrito/api/carrito', ['id_user' => auth()->user()->id]);
 
-         $carritoVistas = $response->body();
+         $carritoVistas = json_decode($response->body(), true);
          return view('carrito.index', compact('carritoVistas'));
 
     }
 
-    public function putCarrito(){
-        $response = Http::withToken('nrDd0neP7q')->put('http://carrito/api/carrito/'. auth()->user()->id, ['id_producto'=> 5, 'cantidad_producto'=>2]);
-        return $response->body();
+    public function putCarrito(Request $request){
+        $response = Http::withToken('nrDd0neP7q')->put('http://carrito/api/carrito/'. auth()->user()->id, ['id_producto'=> $request['id_producto'], 'cantidad_producto'=>$request['cantidad_producto']]);
+
+        return redirect()->route('carrito');
     }
 
 
-    public function postCarrito(){
-        $response = Http::withToken('nrDd0neP7q')->withOptions(['allow_redirects'=>false])->post('http://carrito/api/carrito', ['id_user' => auth()->user()->id, 'id_producto'=> 5,'nombre_producto'=>'pantalon', 'precio_producto' => 20, 'cantidad_producto' => 4]);
-        return $response->body();
+    public function postCarrito(Request $request , Producto $producto){
+
+
+
+        $response = Http::withToken('nrDd0neP7q')->withOptions(['allow_redirects'=>false])->post('http://carrito/api/carrito', ['id_user' => auth()->user()->id, 'id_producto'=> $producto->id,'nombre_producto'=>$producto->nombre, 'precio_producto' => $producto->precio, 'cantidad_producto' => 1]);
+
+        return redirect()->route('carrito');
     }
 
 
-    public function deleteCarrito(){
-        $response = Http::withToken('nrDd0neP7q')->delete('http://carrito/api/carrito/'. auth()->user()->id,['id_producto'=>5]);
-        return $response->body();
+    public function deleteCarrito(Request $request){
+
+        if(isset($request['id_producto'])){
+            //borra una linea del carrito
+            $response = Http::withToken('nrDd0neP7q')->delete('http://carrito/api/carrito/'. auth()->user()->id,['id_producto'=>$request['id_producto']]);
+
+        }else{
+            //borra el carrito entero
+            $response = Http::withToken('nrDd0neP7q')->delete('http://carrito/api/carrito/'. auth()->user()->id);
+
+        }
+
+        return redirect()->route('carrito');
     }
 }
 
